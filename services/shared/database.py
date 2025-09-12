f45bfcea60c3aecc8905dbc4886471c5edd6c1f6
Dinah <dinah.navas@gmail.com>
2025-09-12 16:52:23 -0300

    Correção mapeamento das portas db

--- services/shared/database.py
+++ services/shared/database.py
@@ -1,7 +1,16 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "postgresql://ecommerce:my_password@ecommerce-db:5432/ecommerce"
+#DATABASE_URL = "postgresql://ecommerce:my_password@toxiproxy:5432/ecommerce"
+
+# users/database.py
+DATABASE_URL = "postgresql+psycopg2://ecommerce:my_password@toxiproxy:8475/ecommerce"
+
+# products/database.py
+DATABASE_URL = "postgresql+psycopg2://ecommerce:my_password@toxiproxy:8476/ecommerce"
+
+# orders/database.py
+DATABASE_URL = "postgresql+psycopg2://ecommerce:my_password@toxiproxy:8477/ecommerce"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
